80a9adf9d5e680ecded49298b8a4da2e9a5dca09
wayflayer <91454172+wayflayer@users.noreply.github.com>
2024-05-14 18:09:19 +0300

    Delete src directory

--- src/main/java/com/example/demo/Repository/AllowRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.demo.Repository;
-
-import com.example.demo.model.Allow;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AllowRepository extends JpaRepository<Allow, Long> {
-}
